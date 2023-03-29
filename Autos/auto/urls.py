6e1afe31bd60c1328851708fdaa9f0274834551d
kimenyu <njorogekimenyu@gmail.com>
2023-03-29 19:28:00 +0300

    CRUD

---
+++ Autos/auto/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('register/', views.registerpage, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logoutUser, name = 'logout'),
+    path('view_auto/', views.view_make, name='autos'),
+    path('add_make/', views.MakeCreateView.as_view(), name='create'),
+    path('list_view/', views.MakeListView.as_view(), name='make-list'),
+    path('make_delete/<int:pk>/delete/', views.MakeDeleteView.as_view(), name='delete'),
+    path('make/<int:pk>/update/', views.MakeUpdateView.as_view(), name='update'),
+    # path('list_view/', views.MakeListView.as_view(context_object_name='make_list'), name='list_view'),
+    # path('create/', views.MakeCreateView.as_view(), name='create'),
+]
---
+++ Autos/auto/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from .forms import Createform
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from auto.models import Make
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+
+
+
+
+# Create your views here.
+
+def home(request):
+	return render(request, 'auto/main.html')
+
+def index(request):
+	return render(request, 'auto/crud.html')
+
+def registerpage(request):
+	form = Createform(request.POST or None)
+	context = {'form': form}
+	if form.is_valid():
+		user = form.save()
+		return redirect('login')
+	return render(request, 'auto/register.html', context)
+
+def login_view(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(request, username = username, password = password)
+		login(request, user)
+		return redirect('home')
+	return render(request, 'auto/login.html')#context)
+
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
+
+
+def view_make(request):
+	return render(request, 'auto/autos.html')
+
+class MakeCreateView(CreateView):
+    model = Make
+    fields = '__all__'
+    success_url = reverse_lazy('autos')
+    def form_valid(self, form):
+    	form.instance.user = self.request.user 
+    	return super().form_valid(form)
+
+def list_makes(request):
+	return render(request, 'auto/list_autos.html')
+
+class MakeListView(ListView):
+	model = Make
+	context_object_name = 'make_list'
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		return context
+
+
+
+class MakeDeleteView(DeleteView):
+    model = Make
+    success_url = reverse_lazy('autos')
+
+class MakeUpdateView(UpdateView):
+    model = Make
+    fields = '__all__'
+    context_object_name = 'make_list'
+    success_url = reverse_lazy('autos')
